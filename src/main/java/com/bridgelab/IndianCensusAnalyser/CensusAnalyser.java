4f54ef601ddc43602cd27fec95019b40c21253f3
nitis <nitish8152@gmail.com>
2022-11-09 18:26:25 +0530

    Welcome Indian Census Analyser

---
+++ src/main/java/com/bridgelab/IndianCensusAnalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelab.IndianCensusAnalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welocme To Indian Census Analyser");
+    }
+}
